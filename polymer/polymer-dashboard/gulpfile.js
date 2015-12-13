335204bf4b57326f809c570c11367cca4b79d534
Hugo Queiros <hugofqueiros@gmail.com>
2015-12-13 01:32:10 +0100

    feat(polymer): dashboard: gulpfile ready

--- polymer/polymer-dashboard/gulpfile.js
+++ polymer/polymer-dashboard/gulpfile.js
@@ -185,9 +185,111 @@ gulp.task('vulcanize', function() {
 		.pipe($.size({title: 'vulcanize'}));
 });
 
-gulp.task('default', ['clean'], function(cb) {
-	runSequence();
+/**
+ * not run on default - to enable service workers
+ * See https://github.com/PolymerElements/polymer-starter-kit#enable-service-worker-support
+ * for more context
+ */
+gulp.task('cache-config', function(callback) {
+	var dir = dist();
+	var config = {
+		cacheId: packageJson.name || path.basename(__dirname),
+		disabled: false
+	};
+
+	glob([
+			'index.html',
+			'./',
+			'bower_components/webcomponentsjs/webcomponents-lite.min.js',
+			'{elements,scripts,styles}/**/*.*'],
+		{cwd: dir}, function(error, files) {
+			if (error) {
+				callback(error);
+			} else {
+				config.precache = files;
+
+				var md5 = crypto.createHash('md5');
+				md5.update(JSON.stringify(config.precache));
+				config.precacheFingerprint = md5.digest('hex');
+
+				var configPath = path.join(dir, 'cache-config.json');
+				fs.writeFile(configPath, JSON.stringify(config), callback);
+			}
+		});
 });
+
+gulp.task('clean', function() {
+	return del(['.tmp', dist()]);
+});
+
+gulp.task('serve', ['lint', 'styles', 'elements', 'images'], function() {
+	browserSync({
+		port: 5000,
+		notify: false,
+		logPrefix: 'PSK',
+		snippetOptions: {
+			rule: {
+				match: '<span id="browser-sync-binding"></span>',
+				fn: function (snippet) {
+					return snippet;
+				}
+			}
+		},
+		server: {
+			baseDir: ['.tmp', 'app'],
+			middleware: [historyApiFallback()]
+		}
+	});
+
+	gulp.watch(['app/**/*.html'], reload);
+	gulp.watch(['app/styles/**/*.css'], ['styles', reload]);
+	gulp.watch(['app/elements/**/*.css'], ['elements', reload]);
+	gulp.watch(['app/{scripts,elements}/**/{*.js,*.html}'], ['lint']);
+	gulp.watch(['app/images/**/*'], reload);
+});
+
+gulp.task('serve:dist', ['default'], function() {
+	browserSync({
+		port: 5001,
+		notify: false,
+		logPrefix: 'PSK',
+		snippetOptions: {
+			rule: {
+				match: '<span id="browser-sync-binding"></span>',
+				fn: function(snippet) {
+					return snippet;
+				}
+			}
+		},
+		server: dist(),
+		middleware: [historyApiFallback()]
+	});
+});
+
+gulp.task('default', ['clean'], function(cb) {
+	// Uncomment 'cache-config' if you are going to use service workers.
+	runSequence(
+		['copy', 'styles'],
+		'elements',
+		['lint', 'images', 'fonts', 'html'],
+		'vulcanize', // 'cache-config',
+		cb);
+});
+
+gulp.task('deploy-gh-pages', function() {
+	return gulp.src(dist('**/*'))
+		.pipe($.if(process.env.TRAVIS === 'true', $.ghPages({
+			remoteUrl: '',
+			silent: false,
+			branch: 'gh-pages'
+		}), $.ghPages()));
+});
+
+/**
+ * Load tasks for web-component-tester
+ * Adds tasks for gulp test:local and gulp test:remote
+ */
+require('web-component-tester').gulp.init(gulp);
 
 // Load custom tasks from the tasks directory
 try {
