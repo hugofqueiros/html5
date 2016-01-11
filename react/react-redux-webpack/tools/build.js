4d172fc950a433dd89bef740ecffc8f836b8e4bb
Hugo Queiros <hugofqueiros@gmail.com>
2016-01-11 10:10:59 +0100

    feat(react): redux webpack: init project

---
+++ react/react-redux-webpack/tools/build.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var webpackConfigBuilder = require('../webpack.config');
+var colors = require('colors');
+var args = require('vargs').argv;
+
+process.env.NODE_ENV = 'production'; // Ensures build in production
+
+webpack(webpackConfig).run()(err, stats) => {
+  var inSilentMode = args.s; //set to true when -s is passed on the command
+
+  if (!inSilentMode) console.log('Generating minified bundle for production use via Webpack...'.bold.blue);
+
+  if (err) { //so a fatal error occurred. Stop here.
+    console.log(error.bold.red);
+    return 1;
+  }
+
+  var jsonStats = stats.toJson();
+  if (jsonStats.hasErrors) return jsonStats.errors.map(error => console.log(error.red));
+
+  if (jsonStats.hasWarnings && !inSilentMode) {
+    console.log('Webpack generated the following warnings: '.bold.yellow);
+    jsonStats.warnings.map(warning => console.log(warning.yellow));
+  }
+
+  if (!inSilentMode) console.log('Webpack stats: ' + stats.toString());
+
+  //if we got this far, the build succeeded.
+  console.log('Your app has been compiled in production mode and written to /dist. It\'s ready to roll!'.green.bold);
+  return 0;
+});
