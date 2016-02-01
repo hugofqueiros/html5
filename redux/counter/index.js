3de39b009a14060c1c6e4c5d603e33334810ad14
Hugo Queiros <hugofqueiros@gmail.com>
2016-02-01 10:56:32 +0100

    refactor(redux): wip on counter example

---
+++ redux/counter/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by hfq on 31/01/16.
+ */
---
+++ redux/counter/webpack.config.js
@@ -0,0 +1,39 @@
+/**
+ * Created by hfq on 31/01/16.
+ */
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	devtool: 'cheap-module-eval-source-map',
+	entry: [
+		'webpack-hot-middleware/client',
+		'./index'
+	],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/static'
+	},
+	plugins: [
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	],
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loaders: [ 'babel' ],
+				exclude: /node_modules/,
+				include: __dirname
+			}
+		]
+
+	}
+}
+
+// When inside Redux repo, preger src to compiled version,
+// You can safely delete these line from the project.
+var reduxSrc = path.join(__dirname, '..', '..', 'src')
+
