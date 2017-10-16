ae45fe0490626b396d71426cfed253b7255c75cf
Hugo Queiros <hugofqueiros@gmail.com>
2017-10-16 18:52:42 +0200

    wip

--- graphql/songs-app/client/index.js
+++ graphql/songs-app/client/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App';
+import SongCreate from './components/SongCreate';
 import SongList from './components/SongList';
 
 const client = new ApolloClient({});
@@ -9,10 +13,12 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>
-        Song App
-        <SongList />
-      </div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="song/new" component={SongCreate} />
+        </Route>
+      </Router>
     </ApolloProvider>
   )
 };
