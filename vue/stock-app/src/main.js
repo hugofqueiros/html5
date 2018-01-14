a078c4275c9556d6fa07644ce1beb24fd3dc998b
Hugo Queiros <hugofqueiros@gmail.com>
2018-01-14 20:27:17 +0100

    refactor(vue): stock app

--- vue/stock-app/src/main.js
+++ vue/stock-app/src/main.js
@@ -1,12 +1,23 @@
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import App from './App';
 import router from './router';
+import store from './store/store';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vue-js-stock-trader-7fe39.firebaseio.com/';
+
+Vue.filter('currency', (value) => {
+  return `$ + ${value.toLocaleString()}`;
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App),
 });
--- vue/stock-app/src/router/index.js
+++ vue/stock-app/src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+
+import Home from '../components/Home';
+import Portfolio from '../components/portfolio/Portfolio';
+import Stocks from '../components/stocks/Stocks';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      component: Home,
+    }, {
+      path: '/portfolio',
+      component: Portfolio,
+    }, {
+      path: '/stocks',
+      component: Stocks,
     },
   ],
 });
