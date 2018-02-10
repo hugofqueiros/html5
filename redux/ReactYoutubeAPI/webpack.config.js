ffe46504b8f4d32e9df30057b1ca577ab1478dbb
Hugo Queiros <hugofqueiros@gmail.com>
2018-02-10 20:45:48 +0100

    refactor(redux): app with youtube search

---
+++ redux/ReactYoutubeAPI/webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  devServer: {
+    inline:true,
+    port: 8008
+  },
+  entry: {
+    javascript: './src/index.js'
+  },
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
